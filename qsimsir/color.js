14b955a06d1eb45bdc69a34c771432abdd26709e
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-09-04 21:35:27 +0530

    color

---
+++ qsimsir/color.js
@@ -0,0 +1,9 @@
+
+let head=document.getElementById('demo');
+//  head.style.background = "red",head.style.color = "yellow";
+
+
+// csstext use when we want wright css in one line we use csstext; 
+
+head.style.cssText="color:red;padding:30px;background-color:yellow;"
+document.getElementById("head").cli
