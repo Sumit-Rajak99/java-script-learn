a22cf74010345ffa7e2e2f4ac6e3a0db3941c2d3
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-09-27 22:51:02 +0530

    fetchapi

--- qsimsir/fetch.js
+++ qsimsir/fetch.js
@@ -1,50 +1,18 @@
-// async function access(){
-//         let data = await fetch('https://jsonplaceholder.typicode.com/comments')
-//         let res =await data.json()
-//         console.log(res) 
+async function access(){
+        let data = await fetch('https://jsonplaceholder.typicode.com/comments')
+        let res =await data.json()
+        let apidata=res.map((e)=>`
+        <tr>
+        <td>${e.id}</td>
+        <td>${e.name}</td>
+        <td>${e.email}</td>
+        <td>${e.body}</td>
+        </tr>
+        `).join("")
+        document.querySelector('#showdata').innerHTML=apidata
 
 
-
-
-//     }
-//     access()
-
-
-// let shdata=document.querySelector('#showdata')
-// let finalres=emp.map((obj)=>`
-// <tr>
-// <td> ${obj.name}</td>
-// <td> ${obj.city}</td>
-// <td> ${obj.contact}</td>
-// </tr>
-// `).join("")
-
-// shdata.innerHTML=finalres
-// fetch.js
-async function fetchData() {
-      try {
-        let response = await fetch('https://jsonplaceholder.typicode.com/users');
-        let data = await response.json();
-
-        let tbody = document.getElementById('showdata');
-        tbody.innerHTML = '';
-
-        data.forEach(user => {
-          let row = `
-            <tr>
-              <td>${user.name}</td>
-              <td>${user.phone}</td>
-              <td>${user.address.city}</td>
-            </tr>`;
-          tbody.innerHTML += row;
-        });
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
     }
+    access()
 
-    
-    
-
-fetchData();
 
