903e70dcdcca495777ad0ad9f906618dc17612e3
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-10-06 21:01:18 +0530

    curdopration

---
+++ Labmanual/curd.js
@@ -0,0 +1,15 @@
+// set item
+localStorage.setItem("Name","sumit")
+localStorage.setItem("age",18)
+localStorage.setItem("city","bhopal")
+localStorage.setItem("contact",9926381513)
+
+// getitem 
+let ans = localStorage.getItem('Name');
+alert(ans);
+
+// remove item
+localStorage.removeItem('contact')
+
+// clear
+// localStorage.clear()
